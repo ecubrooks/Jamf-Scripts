8d1624c04a6282fae600a2ce1dec0dfdd43cb516
Brooks Person <personj@RH-PERSONJ.intra.ecu.edu>
2025-08-29 14:49:10 -0400

    Software Update script
    updated script

--- softwareupdate-swiftdialog/softwareupdate-swiftdialog.sh
+++ softwareupdate-swiftdialog/softwareupdate-swiftdialog.sh
@@ -16,7 +16,7 @@
 # - SwiftDialog timeout is treated as a deferral
 #
 # Author: Brooks Person
-# Last Updated: 2025-07-10
+# Last Updated: 2025-07-10, 2025-08-29
 ################################################################
 
 
